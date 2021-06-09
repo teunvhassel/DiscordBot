fa8fe457841f3d36f5303817cc4b09cab185a7cd
Droogkloter <74905591+teunvhassel@users.noreply.github.com>
2021-06-09 20:49:49 +0200

    Update bot.py
    Veranderingen:
    - Commands kunnen nu gebruikt worden door "!" te gebruiken. Bijv: "!gevoelens Hoe gaat het Jos?"
    - Mediabestanden kunnen nu verstuurd worden via de bot door middel van "!weekend"
    - Mediabestanden kunnen nu x aantal keer verstuurd worden via de spam command: "spam x"

--- bot.py
+++ bot.py
@@ -1,16 +1,18 @@
 import discord
 import random
-from discord.ext import commands
+from discord.ext import commands, tasks
 import urllib.parse, urllib.request, re
 import aiohttp
 import asyncio
 import os
+import schedule
+import time
 
 # API key wordt opgehaald vanuit een environment variable
 bot_jos = os.environ.get('bot_jos')
 
 # Met de variabele "client" maak ik het voor mezelf gemakkelijker om de bot op te roepen
-client = commands.Bot(command_prefix='.')
+client = commands.Bot(command_prefix='!')
 
 
 @client.event
@@ -55,6 +57,32 @@ async def youtube(ctx, *, search):
     )
     search_results = re.findall( r"watch\?v=(\S{11})", html_content.read().decode())
     await ctx.send('http://www.youtube.com/watch?v=' + search_results[0]) # als je het getal van de list verhoogt krijg je meer zoekresultaten, 0 = 1 resultaat
+
+# Haal een media bestand op
+@client.command()
+async def weekend(ctx):
+    video = discord.File("./media/royalistic.mp4")
+    await ctx.send(file=video, content="FIJN WEEKEND!!!")
+
+@client.command()
+async def spam(ctx, aantal:int):
+    for i in range(aantal):
+        video = discord.File("./media/royalistic.mp4")
+        await ctx.send(file=video)
+
+def job():
+    print('Niet storen, ik ben aan het werk...')
+
+#@tasks.loop(seconds=10.0)
+#async def called_once_a_day():
+#    await ctx.send("Your message")
+#
+#@called_once_a_day.before_loop
+#async def before():
+#    await bot.wait_until_ready()
+#    print("Finished waiting")
+#
+#called_once_a_day.start()
 
 
 client.run(bot_jos)
