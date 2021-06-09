16a6dd96526fd43d1607c61f2dfffdd797ec6c7e
Droogkloter <74905591+teunvhassel@users.noreply.github.com>
2021-06-09 17:15:26 +0200

    Update bot.py
    In deze versie kan de bot een youtube video ophalen in de chat, dit is te doen door het commando ".youtube [naam video]' in te voeren.
    Ook heb ik een stukje security toegepast voor mijn code. De API key van mijn bot is nu in een virtuele omgeving verstopt zodat deze niet zichtbaar is voor iedereen die mijn script bekijkt.

--- bot.py
+++ bot.py
@@ -1,7 +1,15 @@
 import discord
 import random
 from discord.ext import commands
+import urllib.parse, urllib.request, re
+import aiohttp
+import asyncio
+import os
 
+# API key wordt opgehaald vanuit een environment variable
+bot_jos = os.environ.get('bot_jos')
+
+# Met de variabele "client" maak ik het voor mezelf gemakkelijker om de bot op te roepen
 client = commands.Bot(command_prefix='.')
 
 
@@ -9,31 +17,44 @@ client = commands.Bot(command_prefix='.')
 async def on_ready():
     print('Jos komt binnen via de achterdeur...')
 
-
 @client.event
 async def on_member_join(member):
     print(f'{member} is binnen gekomen via de voordeur...')
-
 
 @client.event
 async def on_member_remove(member):
     print(f'{member} is weggejaagd van de server... mag ik een F in de chat?')
 
 
+
 # Maak een commando aan
 @client.command()
-async def Jos(ctx):
+async def jos(ctx):
     await ctx.send(f'Stop {round(client.latency * 1000)}ms')
 
-
-@client.command(opties=['hoe', 'test'])
+# Vraag aan Jos hoe het gaat
+@client.command(opties=['hoe gaat het', 'test'])
 async def gevoelens(ctx, *, vraag):
     antwoorden = ['Het ging eigenlijk best wel goed totdat jij in deze server kwam.',
                   'Boeie',
                   'Ik verdoe mijn tijd niet aan mensen die microsoft teams gebruiken.',
-                  'Wat nou als je een keertje wegloopt van dat toetsenbordje en nooit meer terug komt?,'
+                  'Wat nou als je een keertje wegloopt van dat toetsenbordje en nooit meer terug komt?',
                   'Nou eigenlijk, best wel wauw.',
                   'Het gaat helemaal super! Ik wordt totaal niet gedwongen door code om dit te zeggen!']
     await ctx.send(f'Vraag: {vraag}\nAntwoord: {random.choice(antwoorden)}')
 
-client.run('ODQ0OTA4Mzg1MTQ0MTQzOTAz.YKZQQA.qnXmNJocbyVMjHtsish1itNS1PA')
+
+# Haal een video van YouTube op
+@client.command()
+async def youtube(ctx, *, search):
+    query_string = urllib.parse.urlencode({
+        'search_query': search
+    })
+    html_content = urllib.request.urlopen(
+        'http://www.youtube.com/results?' + query_string
+    )
+    search_results = re.findall( r"watch\?v=(\S{11})", html_content.read().decode())
+    await ctx.send('http://www.youtube.com/watch?v=' + search_results[0]) # als je het getal van de list verhoogt krijg je meer zoekresultaten, 0 = 1 resultaat
+
+
+client.run(bot_jos)
